ca0abfb1588f17e91ac61264c4c0bd2779cb1cd9
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-03-02 04:45:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ require "pry"
 def oxford_comma(array)
   if array.size == 1
     return array[0]
-  elsif array.size == 2
+  elsif array.size == 3
     return array.join(" and ")
   else
     return array[0..-2].join(', ') + ", and " + array[-1]
